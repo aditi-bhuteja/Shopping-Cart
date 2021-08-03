3b85796bd12d53684002d22beb8fc23440ca38e0
ADITI BHUTEJA <bhuteja.1@iitj.ac.in>
2021-08-04 00:55:59 +0530

    Initial Commit

---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: 'Square Lamp',
+    price: 499,
+    currency: 'Rs.',
+    image: 'images/lamp1.jpg',
+  },
+  {
+    id: 2,
+    name: 'Wooden Table',
+    price: 990,
+    currency: 'Rs.',
+    image: 'images/table1.jpg',
+  },
+  {
+    id: 4,
+    name: 'Asthetic Pot',
+    price: 250,
+    currency: 'Rs.',
+    image: 'images/pot1.jpg',
+  },
+  {
+    id: 6,
+    name: 'Bird Chime',
+    price: 190,
+    currency: 'Rs.',
+    image: 'images/chime1.jpg',
+  },
+  {
+    id: 5,
+    name: 'Sessel Peacock',
+    price: 12000,
+    currency: 'Rs.',
+    image: 'images/05.jpg',
+  },
+  {
+    id: 3,
+    name: 'Interior Painting Frame',
+    price: 850,
+    currency: 'Rs.',
+    image: 'images/frame1.jpg',
+  }
+];
+
+export default data;
