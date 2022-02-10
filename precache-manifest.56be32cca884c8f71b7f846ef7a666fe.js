7cc650de14f62e46bb1a246b62e25bba4480f414
Aditi <bhuteja.1@iitj.ac.in>
2022-02-10 15:56:15 +0530

    Updates

---
+++ precache-manifest.56be32cca884c8f71b7f846ef7a666fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68fba40126eda56ba4d450b6b4baf4a4",
+    "url": "/Shopping-Cart/index.html"
+  },
+  {
+    "revision": "e10934f7ea325a6f5742",
+    "url": "/Shopping-Cart/static/css/2.4a496eec.chunk.css"
+  },
+  {
+    "revision": "ba1041c7149a016bf756",
+    "url": "/Shopping-Cart/static/css/main.07216b04.chunk.css"
+  },
+  {
+    "revision": "e10934f7ea325a6f5742",
+    "url": "/Shopping-Cart/static/js/2.383965ee.chunk.js"
+  },
+  {
+    "revision": "23900771a863482f56e68d68cf1f3072",
+    "url": "/Shopping-Cart/static/js/2.383965ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba1041c7149a016bf756",
+    "url": "/Shopping-Cart/static/js/main.f14812b4.chunk.js"
+  },
+  {
+    "revision": "74501a7cdec45518a468",
+    "url": "/Shopping-Cart/static/js/runtime-main.33718aee.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Shopping-Cart/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Shopping-Cart/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Shopping-Cart/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Shopping-Cart/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Shopping-Cart/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
