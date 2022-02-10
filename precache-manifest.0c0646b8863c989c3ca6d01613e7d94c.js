65cbbb9f38c547eedabb48092512fe2880bc9b8c
Aditi <bhuteja.1@iitj.ac.in>
2022-02-10 14:31:43 +0530

    Updates

---
+++ precache-manifest.0c0646b8863c989c3ca6d01613e7d94c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2225d35f20861f1ce885883889924cb2",
+    "url": "/Shopping-Cart.git/index.html"
+  },
+  {
+    "revision": "e7a92c9e857c92cd8cb9",
+    "url": "/Shopping-Cart.git/static/css/2.5ed22265.chunk.css"
+  },
+  {
+    "revision": "ba1041c7149a016bf756",
+    "url": "/Shopping-Cart.git/static/css/main.07216b04.chunk.css"
+  },
+  {
+    "revision": "e7a92c9e857c92cd8cb9",
+    "url": "/Shopping-Cart.git/static/js/2.383965ee.chunk.js"
+  },
+  {
+    "revision": "23900771a863482f56e68d68cf1f3072",
+    "url": "/Shopping-Cart.git/static/js/2.383965ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba1041c7149a016bf756",
+    "url": "/Shopping-Cart.git/static/js/main.f14812b4.chunk.js"
+  },
+  {
+    "revision": "868b8d4d37703a3a10b3",
+    "url": "/Shopping-Cart.git/static/js/runtime-main.7b4446e2.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Shopping-Cart.git/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Shopping-Cart.git/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Shopping-Cart.git/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Shopping-Cart.git/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Shopping-Cart.git/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.254ba0cc9e547853171a5c367abebb2c.js"
+  "/Shopping-Cart.git/precache-manifest.0c0646b8863c989c3ca6d01613e7d94c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Shopping-Cart.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
